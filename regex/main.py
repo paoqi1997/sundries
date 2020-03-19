2636e737d3f9e10a85b4eae6aa423f7612e04c81
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2020-03-19 16:48:29 +0800

    微调

--- regex/main.py
+++ regex/main.py
@@ -14,23 +14,27 @@ def findAllText(sPattern, sText):
     return results
 
 if __name__ == '__main__':
+    # 匹配日期
     sText = 'Text: 1970-01-01'
     print(groupText(getDatePattern(), sText))
 
-    sText = 'Text: 2015-02-28'
+    # 匹配日期_v2
+    sText = 'Text: 2015-09-01'
     print(groupText(getDatePattern_v2(), sText))
-
     sText = 'Text: 2008-02-29'
     print(groupText(getDatePattern_v2(), sText))
 
+    # 匹配时间
     sText = 'Text: 00:30:00'
     print(groupText(getClockPattern(), sText))
 
+    # 匹配整数
     sText = 'Text: -50m|100m|200m|400m'
     for result in findAllText(getIntPattern(), sText):
         print(result, end=' ')
     print()
 
+    # 匹配浮点数
     sText = 'Text: -0.33|0.66|0.99|2.33'
     for result in findAllText(getFloatPattern(), sText):
         print(result, end=' ')
