c01516561529baaf6a6ef1936ff37e1f969fc14c
paoqi1997 <604869221@qq.com>
2020-08-14 19:54:41 +0800

    docker: listtags.py移除SyncWorker

--- docker/listtags.py
+++ docker/listtags.py
@@ -22,62 +22,6 @@ def writeToFile(sTag, lstTag):
     with open(os.path.join(path[0], sFileName), 'w') as oFile:
         oFile.writelines('\n'.join(lstTag))
 
-class SyncWorker:
-    def __init__(self):
-        self.m_oSession = requests.Session()
-        self.m_Timeout = 10
-
-    def run(self, lstTag):
-        for sTag in lstTag:
-            self.pullTag(sTag)
-
-    def getToken(self, sTag):
-        dParams = {
-            'service': 'registry.docker.io',
-            'scope': 'repository:library/%s:pull'%sTag
-        }
-        oRep = self.m_oSession.get(
-            url='https://auth.docker.io/token', params=dParams, timeout=self.m_Timeout
-        )
-        dResult = oRep.json()
-        sToken = dResult['token']
-        printInfo('Get token.')
-        return sToken
-
-    def pullTag(self, sTag):
-        sToken = self.getToken(sTag)
-        # request v2 api
-        dHeaders = {
-            'Authorization': 'Bearer %s'%sToken
-        }
-        try:
-            oRep = self.m_oSession.get(
-                url='https://registry.hub.docker.com/v2/library/%s/tags/list'%sTag,
-                headers=dHeaders, timeout=self.m_Timeout
-            )
-        except Exception:
-            printInfo('Request v2 api failed, tag: %s'%sTag)
-            return
-
-        if oRep.status_code == 200:
-            printInfo('Request v2 api succ, tag: %s'%sTag)
-            dResult = oRep.json()
-            lstTag = dResult['tags']
-            writeToFile(sTag, lstTag)
-        else:
-            # request v1 api
-            oRep = self.m_oSession.get(
-                url='https://registry.hub.docker.com/v1/repositories/%s/tags'%sTag, timeout=self.m_Timeout
-            )
-            if oRep.status_code == 200:
-                printInfo('Request v1 api succ, tag: %s'%sTag)
-                sResult = '{\"result\": %s}'%oRep.text
-                dResult = json.loads(sResult)
-                lstTag = [dElement['name'] for dElement in dResult['result']]
-                writeToFile(sTag, lstTag)
-            else:
-                printInfo('None.')
-
 class AsyncWorker:
     def __init__(self):
         self.m_Looper = asyncio.get_event_loop()
@@ -106,7 +50,7 @@ class AsyncWorker:
                 ) as oRep:
                     dResult = await oRep.json()
                     sToken = dResult['token']
-                    printInfo('Get token.')
+                    printInfo('Get token succ, tag: %s'%sTag)
                     return sToken
             except asyncio.TimeoutError:
                 printInfo('Failed to get token, tag: %s'%sTag)
@@ -132,7 +76,7 @@ class AsyncWorker:
                     else:
                         await self.reqV1API(sTag)
             except asyncio.TimeoutError:
-                printInfo('Request v2 api failed, tag: %s'%sTag)
+                printInfo('Failed to request v2 api, tag: %s'%sTag)
 
     async def reqV1API(self, sTag):
         async with aiohttp.ClientSession(timeout=self.m_Timeout) as oSession:
@@ -147,24 +91,23 @@ class AsyncWorker:
                     lstTag = [dElement['name'] for dElement in dResult['result']]
                     writeToFile(sTag, lstTag)
                 else:
-                    printInfo('None.')
+                    printInfo('Failed to request v1 api, tag: %s'%sTag)
 
 if __name__ == '__main__':
-    print(argv)
-    if len(argv) <= 2:
-        print('Usage: python3 main.py <useAsync> <name>')
+    sHelp = '''Usage:
+    python3 main.py <tagname(s)>
+Examples:
+    python3 main.py nginx
+    python3 main.py nginx,redis'''
+    if len(argv) <= 1:
+        print(sHelp)
         exit(1)
 
     printInfo('Begin.')
 
-    bAsyncFlag = bool(int(argv[1]))
-    lstTagName = argv[2].split(',')
+    lstTagName = argv[1].split(',')
 
-    if bAsyncFlag:
-        oAsyncWorker = AsyncWorker()
-        oAsyncWorker.run(lstTagName)
-    else:
-        oSyncWorker = SyncWorker()
-        oSyncWorker.run(lstTagName)
+    oAsyncWorker = AsyncWorker()
+    oAsyncWorker.run(lstTagName)
 
     printInfo('End.')
