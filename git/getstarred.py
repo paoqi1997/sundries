a7d2b73f4cb03f8a3b981e80f07d51aeeb80c3ac
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2022-01-23 19:33:03 +0800

    fix(git): add getstarred.py

---
+++ git/getstarred.py
@@ -0,0 +1,41 @@
+from sys import argv, path
+
+import json
+import os
+import platform
+
+if __name__ == '__main__':
+    if len(argv) <= 1:
+        sUserName = 'paoqi1997'
+    else:
+        sUserName = argv[1]
+
+    # https://docs.github.com/cn/rest/reference/activity#list-repositories-starred-by-a-user
+    sUrl = f'https://api.github.com/users/{sUserName}/starred'
+
+    lstOption = [
+        '-s',
+        '-H',
+        '\"Accept: application/vnd.github.v3+json\"'
+    ]
+
+    # https://curl.se/mail/lib-2016-03/0202.html
+    if platform.system() == 'Windows':
+        lstOption.append('--ssl-no-revoke')
+
+    sOptions = ' '.join(lstOption)
+
+    sCmd = f'curl {sOptions} {sUrl}'
+
+    with os.popen(sCmd) as oPipe:
+        sResult = oPipe.buffer.read().decode('utf-8')
+        lstRepo = json.loads(sResult)
+
+        dOutput = { 'repos': lstRepo }
+        sOutput = json.dumps(dOutput, indent=2)
+
+        sFileName = f'{sUserName}.starred.json'
+        sFilePath = os.path.join(path[0], sFileName)
+
+        with open(sFilePath, 'w') as oFile:
+            oFile.write(f'{sOutput}\n')
